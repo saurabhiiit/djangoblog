f1ae9171491836f22149ce069a0e8036007edfa4
saurabh agrahari <saurabhagrahari487@gmail.com>
2015-12-26 09:34:31 +0530

    Models have been created for our blog application

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post, Author, Category, Tag	
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Tag)
+
+
+
+# Register your models here.
